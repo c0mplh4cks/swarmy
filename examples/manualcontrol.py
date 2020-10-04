1a78318f96fbe6fa8608e59e16eb7e4a226dee7f
c0mplh4cks <jelmer943@gmail.com>
2020-10-04 22:12:34 +0200

    adding examples

---
+++ examples/manualcontrol.py
@@ -0,0 +1,47 @@
+# === Importing Library's === #
+from readchar import readchar
+from time import sleep
+from sys import path
+
+path.insert(0, "/home/c0mplh4cks/coding/swarmy/client/")
+
+import swarmy_lib as s
+
+
+
+# === Main === #
+speed = 1000
+turnspeed = 300
+
+devices = [
+    ("rapidice", 8888),
+]
+
+
+
+
+try:
+
+    while True:
+
+        key = readchar()
+
+        if key == "\x03": raise KeyboardInterrupt
+        elif key == "w": a, b = speed, speed
+        elif key == "s": a, b = -speed, -speed
+        elif key == "d": a, b = turnspeed, -turnspeed
+        elif key == "a": a, b = -turnspeed, turnspeed
+        elif key == " ": a, b = 0, 0
+
+        for device in devices:
+            s.motor_left(a, device)
+            s.motor_right(b, device)
+
+    sleep(0.01)
+
+
+
+except KeyboardInterrupt:
+    for device in devices:
+        s.motor_left(0, device)
+        s.motor_right(0, device)
